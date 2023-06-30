ac77f11063e1d394c8ea4f130b2d86558481535f
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-30 12:42:59 -0500

    Classes exercise done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Car car = new Car();
+            car.Make = "Toyota";
+            car.Model = "Camy";
+            car.Year = 2023;
+
+            Console.WriteLine($"My car is {car.Make}");
+            Console.WriteLine($" My car model is {car.Model}");
+            Console.WriteLine($"My car year is {car.Year}");
+
         }
     }
 }
